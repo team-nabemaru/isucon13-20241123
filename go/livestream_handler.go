08a69bb025e6e407ee4eb7771216e8ca1afbf948
zr <komatsuryo989@gmail.com>
2024-11-23 16:29:14 +0900

    fix: fillLivestreamResponse to Redis

--- go/livestream_handler.go
+++ go/livestream_handler.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/isucon/isucon13/webapp/go/redis"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
@@ -489,10 +490,9 @@ func getLivecommentReportsHandler(c echo.Context) error {
 //
 //	そのlivestreamModelをもとにLivestreamを作成しています。Livestreamには、オーナー情報、タグ情報が含まれています。
 func fillLivestreamResponse(ctx context.Context, tx *sqlx.Tx, livestreamModel LivestreamModel) (Livestream, error) {
-	ownerModel := UserModel{}
-	if err := tx.GetContext(ctx, &ownerModel, "SELECT * FROM users WHERE id = ?", livestreamModel.UserID); err != nil {
-		return Livestream{}, err
-	}
+	client := redis.NewClient(ctx)
+	userRepository := redis.NewRedisRepository[UserModel](dbConn, *client)
+	ownerModel, err := userRepository.GetById(ctx, strconv.FormatInt(livestreamModel.UserID, 10), "users")
 
 	owner, err := fillUserResponse(ctx, tx, ownerModel)
 	if err != nil {
