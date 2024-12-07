aee2cc3a193bef24a084f0221398440712d9d9b8
nakayan5 <tomoya.progress@gmail.com>
2024-12-07 17:07:50 +0900

    feat: add livestreamCache to main.go

--- go/main.go
+++ go/main.go
@@ -47,6 +47,7 @@ var (
 	iconHashCache       sync.Map
 	imageCache          sync.Map
 	tagsCache           sync.Map
+	livestreamCache     sync.Map
 	reactionsCache      sync.Map
 )
 
@@ -189,6 +190,7 @@ func initializeHandler(c echo.Context) error {
 	iconHashCache = sync.Map{}
 	imageCache = sync.Map{}
 	tagsCache = sync.Map{}
+	livestreamCache = sync.Map{}
 	cacheLock.Unlock()
 
 	return c.JSON(http.StatusOK, InitializeResponse{
